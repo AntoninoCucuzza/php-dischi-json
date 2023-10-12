f46d8e450edb3e2a5483a772bfe4a090d695a22a
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-12 15:17:16 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$test = 'we';
+var_dump($test);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-dischi-json</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+    <div id="app">
+        {{ message }}
+    </div>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./assets/js/main.js"></script>
+</body>
+
+</html>
